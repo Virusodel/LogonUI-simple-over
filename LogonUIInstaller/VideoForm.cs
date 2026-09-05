6980dc7bf8e28e32292ac189c936871c3ff6b34d
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-09-05 10:32:38 +0300

    Update VideoForm.cs

--- LogonUIInstaller/VideoForm.cs
+++ LogonUIInstaller/VideoForm.cs
@@ -6,44 +6,10 @@ namespace HorrorTrojan
 {
     public partial class VideoForm : Form
     {
-        private AxWindowsMediaPlayer videoPlayer;
-
         public VideoForm(string videoPath)
         {
             InitializeComponent();
             LoadVideo(videoPath);
-        }
-
-        private void InitializeComponent()
-        {
-            this.videoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
-            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).BeginInit();
-            this.SuspendLayout();
-
-            // videoPlayer
-            this.videoPlayer.Dock = DockStyle.Fill;
-            this.videoPlayer.Enabled = true;
-            this.videoPlayer.Name = "videoPlayer";
-            this.videoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(new System.ComponentModel.ComponentResourceManager(typeof(VideoForm)).GetObject("videoPlayer.OcxState")));
-            this.videoPlayer.Size = new System.Drawing.Size(800, 600);
-            this.videoPlayer.TabIndex = 0;
-            this.videoPlayer.uiMode = "none";
-
-            // VideoForm
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
-            this.Controls.Add(this.videoPlayer);
-            this.FormBorderStyle = FormBorderStyle.None;
-            this.WindowState = FormWindowState.Maximized;
-            this.TopMost = true;
-            this.ShowInTaskbar = false;
-            this.BackColor = System.Drawing.Color.Black;
-            this.Name = "VideoForm";
-            this.Text = "";
-
-            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).EndInit();
-            this.ResumeLayout(false);
         }
 
         private void LoadVideo(string videoPath)
@@ -56,15 +22,6 @@ namespace HorrorTrojan
                 this.videoPlayer.Ctlcontrols.play();
             }
             catch { }
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                try { videoPlayer?.Dispose(); } catch { }
-            }
-            base.Dispose(disposing);
         }
     }
 }

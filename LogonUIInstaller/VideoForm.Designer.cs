6e5e05bab720436feae23fac933768b048b882bb
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-09-05 12:16:55 +0300

    Update VideoForm.Designer.cs

--- LogonUIInstaller/VideoForm.Designer.cs
+++ LogonUIInstaller/VideoForm.Designer.cs
@@ -3,6 +3,35 @@ namespace HorrorTrojan
     partial class VideoForm
     {
         private System.ComponentModel.IContainer components = null;
+        private System.Windows.Forms.PictureBox pictureBox;
+
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+
+            // pictureBox
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox.BackColor = System.Drawing.Color.Black;
+
+            // VideoForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.pictureBox);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Name = "VideoForm";
+            this.Text = "";
+
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -11,22 +40,6 @@ namespace HorrorTrojan
                 components.Dispose();
             }
             base.Dispose(disposing);
-        }
-
-        private void InitializeComponent()
-        {
-            this.SuspendLayout();
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 600);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
-            this.TopMost = true;
-            this.ShowInTaskbar = false;
-            this.BackColor = System.Drawing.Color.Black;
-            this.Name = "VideoForm";
-            this.Text = "";
-            this.ResumeLayout(false);
         }
     }
 }

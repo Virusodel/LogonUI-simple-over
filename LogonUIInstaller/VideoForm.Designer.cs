c2d1943f5668b3224b01125ef03576a11ae694ea
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-09-05 10:27:09 +0300

    Update VideoForm.Designer.cs

--- LogonUIInstaller/VideoForm.Designer.cs
+++ LogonUIInstaller/VideoForm.Designer.cs
@@ -3,6 +3,7 @@ namespace HorrorTrojan
     partial class VideoForm
     {
         private System.ComponentModel.IContainer components = null;
+        private AxWMPLib.AxWindowsMediaPlayer videoPlayer;
 
         protected override void Dispose(bool disposing)
         {
@@ -15,8 +16,35 @@ namespace HorrorTrojan
 
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            this.videoPlayer = new AxWMPLib.AxWindowsMediaPlayer();
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).BeginInit();
+            this.SuspendLayout();
+
+            // videoPlayer
+            this.videoPlayer.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.videoPlayer.Enabled = true;
+            this.videoPlayer.Name = "videoPlayer";
+            this.videoPlayer.OcxState = ((System.Windows.Forms.AxHost.State)(new System.ComponentModel.ComponentResourceManager(typeof(VideoForm)).GetObject("videoPlayer.OcxState")));
+            this.videoPlayer.Size = new System.Drawing.Size(800, 600);
+            this.videoPlayer.TabIndex = 0;
+            this.videoPlayer.TabStop = false;
+            this.videoPlayer.uiMode = "none";
+
+            // VideoForm
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 600);
+            this.Controls.Add(this.videoPlayer);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.TopMost = true;
+            this.ShowInTaskbar = false;
+            this.BackColor = System.Drawing.Color.Black;
+            this.Name = "VideoForm";
+            this.Text = "";
+
+            ((System.ComponentModel.ISupportInitialize)(this.videoPlayer)).EndInit();
+            this.ResumeLayout(false);
         }
     }
 }

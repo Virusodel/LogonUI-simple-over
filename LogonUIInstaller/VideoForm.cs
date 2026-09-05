984d0849c87b571de772b4c404e7ba25ee16ea44
Virusodel <134794046+Virusodel@users.noreply.github.com>
2026-09-05 13:02:37 +0300

    Update VideoForm.cs

--- LogonUIInstaller/VideoForm.cs
+++ LogonUIInstaller/VideoForm.cs
@@ -8,11 +8,16 @@ namespace HorrorTrojan
     public partial class VideoForm : Form
     {
         private Image gifImage;
+        private bool isDisposed = false;
 
         public VideoForm(string gifPath)
         {
             InitializeComponent();
             LoadGif(gifPath);
+            
+            // Подписываемся на события закрытия
+            this.FormClosing += VideoForm_FormClosing;
+            this.Disposed += (s, e) => { isDisposed = true; };
         }
 
         private void LoadGif(string gifPath)
@@ -24,10 +29,43 @@ namespace HorrorTrojan
                     gifImage = Image.FromFile(gifPath);
                     this.pictureBox.Image = gifImage;
                     this.pictureBox.SizeMode = PictureBoxSizeMode.StretchImage;
-                    ImageAnimator.Animate(gifImage, (s, ev) => { this.pictureBox.Invalidate(); });
+                    ImageAnimator.Animate(gifImage, (s, ev) => 
+                    { 
+                        if (!isDisposed && this.pictureBox != null && !this.pictureBox.IsDisposed)
+                        {
+                            this.pictureBox.Invalidate();
+                        }
+                    });
                 }
             }
             catch { }
+        }
+
+        private void VideoForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            // Останавливаем анимацию при закрытии
+            if (gifImage != null)
+            {
+                ImageAnimator.StopAnimate(gifImage, (s, ev) => { });
+            }
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                isDisposed = true;
+                if (gifImage != null)
+                {
+                    ImageAnimator.StopAnimate(gifImage, (s, ev) => { });
+                    gifImage.Dispose();
+                }
+                if (pictureBox != null && !pictureBox.IsDisposed)
+                {
+                    pictureBox.Image = null;
+                }
+            }
+            base.Dispose(disposing);
         }
     }
 }
